3e9293c66cebd25372db782bce3526f272d9306e
Ryan Drapeau <drapeau@MacBookPro.localdomain>
2024-12-05 21:57:36 -0800

    day 6

--- 2024/5/part_1.py
+++ 2024/5/part_1.py
@@ -1,45 +1,50 @@
 from collections import deque
 
 
-def validate_update(graph, update):
+def validate_update(graph, update, with_correction=False):
 	for j in range(len(update) - 1, 0, -1):
 		cur = update[j]
 		constraints = graph[cur]
 		for i in range(0, j):
 			next = update[i]
 			if next in constraints:
-				return False
-	return True
+				if with_correction:
+					update[j], update[i] = update[i], update[j]
+					return validate_update(graph, update, with_correction=True)
+				else:
+					return False
+
+	return True if not with_correction else update
 
 
-def filter_graph_to_nodes_in_update(graph, update):
-	filtered_graph = {}
-	in_degrees = {node: 0 for node in update}
-	for node in update:
-		filtered_graph[node] = []
-		for neighbor in graph[node]:
-			if neighbor in update:
-				filtered_graph[node].append(neighbor)
-				in_degrees[neighbor] += 1
+# def filter_graph_to_nodes_in_update(graph, update):
+# 	filtered_graph = {}
+# 	in_degrees = {node: 0 for node in update}
+# 	for node in update:
+# 		filtered_graph[node] = []
+# 		for neighbor in graph[node]:
+# 			if neighbor in update:
+# 				filtered_graph[node].append(neighbor)
+# 				in_degrees[neighbor] += 1
 
-	return filtered_graph, in_degrees
+# 	return filtered_graph, in_degrees
 
 
-def correct_update(graph, update):
-	filtered_graph, in_degrees = filter_graph_to_nodes_in_update(graph, update)
-	queue = deque([node for node in in_degrees if in_degrees[node] == 0])
+# def correct_update(graph, update):
+# 	filtered_graph, in_degrees = filter_graph_to_nodes_in_update(graph, update)
+# 	queue = deque([node for node in in_degrees if in_degrees[node] == 0])
 
-	corrected_update = []
-	while len(queue) > 0:
-		cur = queue.popleft()
-		corrected_update.append(cur)
+# 	corrected_update = []
+# 	while len(queue) > 0:
+# 		cur = queue.popleft()
+# 		corrected_update.append(cur)
 
-		for node in filtered_graph[cur]:
-			in_degrees[node] -= 1
-			if in_degrees[node] == 0:
-				queue.append(node)
+# 		for node in filtered_graph[cur]:
+# 			in_degrees[node] -= 1
+# 			if in_degrees[node] == 0:
+# 				queue.append(node)
 
-	return corrected_update
+# 	return corrected_update
 
 
 def validate_updates(graph, updates):
@@ -52,7 +57,7 @@ def validate_updates(graph, updates):
 		if is_valid:
 			running_total += update[len(update) // 2]
 		else:
-			new_update = correct_update(graph, update)
+			new_update = validate_update(graph, update, with_correction=True)
 			new_update_running_total += new_update[len(new_update) // 2]
 
 	return running_total, new_update_running_total
