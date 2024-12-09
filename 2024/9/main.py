c33d091b3d100c7f93d813daa6ea4130f2d6231e
rdrapeau <3769522+rdrapeau@users.noreply.github.com>
2024-12-08 22:12:59 -0800

    shorter

--- 2024/9/main.py
+++ 2024/9/main.py
@@ -1,38 +1,38 @@
 
 def reconstruct_disk(input_string):
-	reconstructed_disk = []
+	disk = []
 	file_sizes = {}
 	blank_sizes = {}
 	id_num = -1
 	for i in range(len(input_string)):
 		item = input_string[i]
 		if i % 2 == 0:
-			file_sizes[len(reconstructed_disk)] = item
+			file_sizes[len(disk)] = item
 			id_num += 1
 		else:
-			blank_sizes[len(reconstructed_disk)] = item
+			blank_sizes[len(disk)] = item
 
 		for j in range(item):
-			reconstructed_disk.append('.' if i % 2 == 1 else id_num)
+			disk.append('.' if i % 2 == 1 else id_num)
 
-	return reconstructed_disk, file_sizes, blank_sizes
+	return disk, file_sizes, blank_sizes
 
 
-def compact_disk_part_1(reconstructed_disk, file_sizes, blank_sizes):
+def compact_disk_part_1(disk, file_sizes, blank_sizes):
 	left = 0
-	right = len(reconstructed_disk) - 1
+	right = len(disk) - 1
 	while left < right:
-		if reconstructed_disk[left] != '.':
+		if disk[left] != '.':
 			left += 1
-		elif reconstructed_disk[right] == '.':
+		elif disk[right] == '.':
 			right -= 1
-		elif reconstructed_disk[right] != '.':
-			reconstructed_disk[left], reconstructed_disk[right] = reconstructed_disk[right], reconstructed_disk[left]
+		elif disk[right] != '.':
+			disk[left], disk[right] = disk[right], disk[left]
 			left += 1
 			right -= 1
 
 
-def compact_disk_part_2(reconstructed_disk, file_sizes, blank_sizes):
+def compact_disk_part_2(disk, file_sizes, blank_sizes):
 	index_with_file_size = sorted([(i, file_sizes[i]) for i in file_sizes], reverse=True)
 	index_with_blank_size = sorted([(i, blank_sizes[i]) for i in blank_sizes])
 
@@ -45,7 +45,7 @@ def compact_disk_part_2(reconstructed_disk, file_sizes, blank_sizes):
 
 			if blank_size >= size:
 				for k in range(size):
-					reconstructed_disk[blank_index + k], reconstructed_disk[index + k] = reconstructed_disk[index + k], reconstructed_disk[blank_index + k]
+					disk[blank_index + k], disk[index + k] = disk[index + k], disk[blank_index + k]
 
 				index_with_blank_size[j] = (blank_index + size, blank_size - size)
 				break
@@ -55,11 +55,11 @@ def main():
 	with open('input.txt') as f:
 		input_string = [int(i) for i in f.read().strip()]
 
-	reconstructed_disk, file_sizes, blank_sizes = reconstruct_disk(input_string)
+	disk, file_sizes, blank_sizes = reconstruct_disk(input_string)
 
-	# compact_disk_part_1(reconstructed_disk, file_sizes, blank_sizes)
-	compact_disk_part_2(reconstructed_disk, file_sizes, blank_sizes)
-	print(sum(i * num for i, num in enumerate(reconstructed_disk) if num != '.'))
+	# compact_disk_part_1(disk, file_sizes, blank_sizes)
+	compact_disk_part_2(disk, file_sizes, blank_sizes)
+	print(sum(i * num for i, num in enumerate(disk) if num != '.'))
 
 
 if __name__ == '__main__':
