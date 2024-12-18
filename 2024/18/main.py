eea327955ac617540f365c7713541a672e7043e5
rdrapeau <3769522+rdrapeau@users.noreply.github.com>
2024-12-17 22:14:47 -0800

    binary search

--- 2024/18/main.py
+++ 2024/18/main.py
@@ -1,8 +1,9 @@
-import heapq
+import heapq, copy
 
-directions = [
-	(1, 0), (0, 1), (-1, 0), (0, -1)
-]
+def fill(grid, points, character):
+	for row, col in points:
+		grid[row][col] = character
+
 
 def has_path(grid, size):
 	start = (0, 0)
@@ -13,7 +14,7 @@ def has_path(grid, size):
 		if pos == (size - 1, size - 1):
 			return True, cost
 
-		for d in directions:
+		for d in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
 			new_pos = (d[0] + pos[0], d[1] + pos[1])
 			if new_pos[0] >= 0 and new_pos[1] >= 0 and new_pos[0] < size and new_pos[1] < size and grid[new_pos[0]][new_pos[1]] != '#' and lowest_cost.get(new_pos, float('inf')) > cost + 1:
 				heapq.heappush(queue, (cost + 1, new_pos))
@@ -22,25 +23,35 @@ def has_path(grid, size):
 	return False, None
 
 
+def binary_search(grid, size, obstacles, left, right):
+	lowest = float('inf')
+	while left < right:
+		new_grid = copy.deepcopy(grid)
+		mid = (left + right) // 2
+		fill(new_grid, obstacles[:mid + 1], '#')
+		if has_path(new_grid, size)[0]:
+			left = mid + 1
+		else:
+			right = mid
+			lowest = min(mid, lowest)
+
+	return lowest
+
+
 def main():
 	with open('input.txt') as f:
 		data = f.readlines()
 		data = [[int(a) for a in x.split(',')] for x in data]
-		data = [(b,a) for a,b in data]
+		obstacles = [(b,a) for a,b in data]
 
 	size = 71
 	grid = [['.' for _ in range(size)] for _ in range(size)]
-	for row,col in data[:1024]:
-		grid[row][col] = '#'
+	fill(grid, obstacles[:1024], '#')
+	print(has_path(grid, size))
 
-	print(has_path(grid, size)[1])
-
-	for row,col in data[1024:]:
-		grid[row][col] = '#'
-		if not has_path(grid, size)[0]:
-			print(col,row)
-			break
-
+	fill(grid, obstacles[:1024], '.')
+	i = binary_search(grid, size, data, 0, len(obstacles))
+	print(i, data[i])
 
 if __name__ == '__main__':
 	main()
