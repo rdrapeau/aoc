a42d91259950d5ddad445f4da0a7ee646c77ea54
rdrapeau <3769522+rdrapeau@users.noreply.github.com>
2024-12-22 21:34:32 -0800

    day 23

--- 2024/23/main.py
+++ 2024/23/main.py
@@ -1,6 +1,18 @@
-
+import networkx as nx
+from itertools import permutations
 
 with open('input.txt') as f:
-	data = [int(x.strip()) for x in f.readlines()]
+	data = [tuple(x.strip().split('-')) for x in f.readlines()]
 
-p1 = 0
+all_edges = set(data + [tuple([x[1], x[0]]) for x in data])
+
+G = nx.Graph()
+G.add_edges_from(all_edges)
+
+print(sum(
+	1 for c in nx.enumerate_all_cliques(G)
+	if len(c) == 3 and any(x.startswith('t') for x in c)
+))
+
+largest = max(nx.find_cliques(G), key=len)
+print(','.join(sorted(largest)))
